13bd563cf5300c584daf98d28ab403cc32906162
befairytale@163.com <befairytale@163.com>
2018-11-04 22:32:54 +0800

    commit

---
+++ artisan/config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  //OPEN_PROXY: false // 是否开启代理, 重置后需重启vue-cli
+  BASE_API: '"http://localhost:8080"',
+})
