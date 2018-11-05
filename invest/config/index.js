e38c2e86cb39f49843e7e4eead7fe349be6a2a9a
wangwei <wangwei1008.ok@163.com>
2018-11-05 14:30:43 +0800

    init invest

--- invest/config/index.js
+++ invest/config/index.js
@@ -20,18 +20,18 @@ module.exports = {
             '^/sys': '/sys'
           }
         },
-        '/quartz': {
-          target: 'http://localhost:1007',
+        '/invest': {
+          target: 'http://localhost:1091',
           changeOrigin: true,
           pathRewrite: {
-            '^/quartz': '/quartz'
+            '^/invest': '/invest'
           }
         }
       },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 1008, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 10081, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
     notifyOnErrors: true,
