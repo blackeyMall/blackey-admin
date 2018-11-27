4d676eab9f0db1deba22c6d63d2b66b4ecbbe307
befairytale@163.com <befairytale@163.com>
2018-11-27 16:54:10 +0800

    commit

---
+++ warmwinter/src/views/modules/artisan/User.vue
@@ -0,0 +1,154 @@
+<template>
+  <div class="mod-user">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <!-- <el-form-item>
+        <el-input v-model="dataForm.userName" placeholder="用户名" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+      </el-form-item> -->
+    </el-form>
+    <el-table :data="dataList" border v-loading="dataListLoading" @selection-change="selectionChangeHandle" :fit="true">
+      <el-table-column type="selection" header-align="center" align="center">
+      </el-table-column>
+      <el-table-column prop="avatarUrl" header-align="center" align="center" label="头像">
+        <template slot-scope="scope">
+          <img :src="scope.row.avatarUrl" alt="" style="width: 100%; max-width: 50px; border-radius: 50%;">
+        </template>
+      </el-table-column>
+      <el-table-column prop="nickName" header-align="center" align="center" label="昵称">
+      </el-table-column>
+      <el-table-column prop="country" header-align="center" align="center" label="国家">
+      </el-table-column>
+      <el-table-column prop="province" header-align="center" align="center" label="省">
+      </el-table-column>
+      <el-table-column prop="city" header-align="center" align="center" label="城市">
+      </el-table-column>
+      <!-- <el-table-column prop="openId" header-align="center" align="center" label="唯一标识">
+      </el-table-column> -->
+      <el-table-column prop="gender" header-align="center" align="center" label="性别">
+      </el-table-column>
+      <el-table-column prop="createdDate" header-align="center" align="center" label="用户注册时间">
+      </el-table-column>
+    </el-table>
+    <el-pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" :current-page="pageIndex" :page-sizes="[10, 20, 50, 100]" :page-size="pageSize" :total="totalPage" layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </div>
+</template>
+
+
+<script>
+import AddOrUpdate from "./config-add-or-update";
+export default {
+  data() {
+    return {
+      dataForm: {
+        userName: ""
+      },
+      dataList: [],
+      pageIndex: 1,
+      pageSize: 10,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: [],
+      addOrUpdateVisible: false
+    };
+  },
+  components: {
+    AddOrUpdate
+  },
+  activated() {
+    this.getDataList();
+  },
+  methods: {
+    // 获取数据列表,params 需要手动添加
+    getDataList() {
+      this.dataListLoading = true;
+      this.$http({
+        url: this.$http.adornUrl("/artisan/user/list/page"),
+        method: "get",
+        params: this.$http.adornParams({
+          page: this.pageIndex,
+          limit: this.pageSize
+        })
+      }).then(({ data }) => {
+        if (data && data.code === 200) {
+          this.dataList = data.data.list;
+          this.totalPage = data.data.totalCount;
+        } else {
+          this.dataList = [];
+          this.totalPage = 0;
+        }
+        this.dataListLoading = false;
+      });
+    },
+    // 每页数
+    sizeChangeHandle(val) {
+      this.pageSize = val;
+      this.pageIndex = 1;
+      this.getDataList();
+    },
+    // 当前页
+    currentChangeHandle(val) {
+      this.pageIndex = val;
+      this.getDataList();
+    },
+    // 多选
+    selectionChangeHandle(val) {
+      this.dataListSelections = val;
+    },
+    // 新增 / 修改
+    addOrUpdateHandle(id) {
+      this.addOrUpdateVisible = true;
+      this.$nextTick(() => {
+        this.$refs.addOrUpdate.init(id);
+      });
+    },
+
+    // 删除
+    deleteHandle(id) {
+      var ids = id
+        ? [id]
+        : this.dataListSelections.map(item => {
+            return item.id;
+          });
+      this.$confirm(
+        `确定对[id=${ids.join(",")}]进行[${id} ? '删除' : '批量删除'}]操作?`,
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning"
+        }
+      )
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/sys/${classname}/delete"),
+            method: "post",
+            data: this.$http.adornData(userIds, false)
+          }).then(({ data }) => {
+            if (data && data.code === 200) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList();
+                }
+              });
+            } else {
+              //需要添加 data.messge
+              this.$message.error();
+            }
+          });
+        })
+        .catch(() => {});
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ warmwinter/src/views/modules/artisan/project-add-or-update.vue
@@ -0,0 +1,104 @@
+<template>
+  <el-dialog :title="!dataForm.id ? '新增' : '修改'" :close-on-click-modal="false" :visible.sync="visible">
+    <el-form :model="dataForm" ref="dataForm" @keyup.enter.native="dataFormSubmit()" label-width="80px">
+      <el-form-item label="项目分类名称" prop="name">
+        <el-input v-model="dataForm.name" placeholder="项目分类名称"></el-input>
+      </el-form-item>
+      <el-form-item label="缩略图" prop="picUrl">
+        <el-upload action="/artisan/file/upload" list-type="picture" :on-success="showImageHandle" style="mar">
+          <img :src="dataForm.picUrl" style="width:50% ;float: left">
+          <el-button size="small" type="primary">点击上传</el-button>
+        </el-upload>
+      </el-form-item>
+      <el-form-item label="项目描述" prop="proDesc">
+        <el-input v-model="dataForm.proDesc" placeholder="项目描述"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      visible: false,
+      dataForm: {
+        id: 0,
+        name: "",
+        picUrl: "",
+        proDesc: ""
+      },
+      showUpload: false
+    };
+  },
+  methods: {
+    init(id) {
+      this.dataForm.id = id;
+
+      this.showUpload = true;
+      this.visible = true;
+      this.$nextTick(() => {
+        this.$refs["dataForm"].resetFields();
+        if (this.dataForm.id) {
+          this.$http({
+            url: this.$http.adornUrl(
+              `/artisan/project/info/${this.dataForm.id}`
+            ),
+            method: "get",
+            params: this.$http.adornParams()
+          }).then(({ data }) => {
+            if (data && data.code === 200) {
+              this.showUpload = false;
+              this.dataForm.name = data.data.name;
+              this.dataForm.picUrl = data.data.picUrl;
+              this.dataForm.proDesc = data.data.proDesc;
+            }
+          });
+        }
+      });
+    },
+    showImageHandle(res) {
+      this.showImage = true;
+      this.showUpload = false;
+      this.dataForm.picUrl = res.data;
+    },
+    // 表单提交
+    dataFormSubmit() {
+      this.$refs["dataForm"].validate(valid => {
+        if (valid) {
+          this.$http({
+            url: this.$http.adornUrl(
+              `/artisan/project/${!this.dataForm.id ? "save" : "update"}`
+            ),
+            method: "post",
+            data: this.$http.adornData({
+              id: this.dataForm.id || undefined,
+              name: this.dataForm.name,
+              picUrl: this.dataForm.picUrl,
+              proDesc: this.dataForm.proDesc
+            })
+          }).then(({ data }) => {
+            if (data && data.code === 200) {
+              this.$message({
+                message: "操作成功",
+                type: "success",
+                duration: 1500,
+                onClose: () => {
+                  this.visible = false;
+                  this.$emit("refreshDataList");
+                }
+              });
+            } else {
+              this.$message.error(data.message);
+            }
+          });
+        }
+      });
+    }
+  }
+};
+</script>
